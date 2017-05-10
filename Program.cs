16000725e879b0ccc96b55f225ac45fb6b021c24
Tri Nguyen <nguyen190887@gmail.com>
2017-05-11 05:17:57 +0700

    Init .net core & Roslyn demo code

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace RoslynCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateSampleViewModel();
+            EmitDemo.GenerateAssembly();
+        }
+
+        static void GenerateSampleViewModel()
+        {
+            const string models = @"namespace Models
+{
+  public class Item
+  {
+    public string ItemName { get; set }
+  }
+}
+";
+            var node = CSharpSyntaxTree.ParseText(models).GetRoot();
+            var viewModel = ViewModelGeneration.GenerateViewModel(node);
+            if (viewModel != null)
+                Console.WriteLine(viewModel.ToFullString());
+            // Console.ReadLine();
+        }
+    }
+}
